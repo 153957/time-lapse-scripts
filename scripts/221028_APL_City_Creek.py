7d13fbd62edd966eac53d41224ad4fbde5aa1384
Arne de Laat <arne@delaat.net>
2022-11-14 09:28:04 +0100

    Add new time-lapse movie of City Creek

---
+++ scripts/221028_APL_City_Creek.py
@@ -0,0 +1,14 @@
+import os
+
+from time_lapse import make_movie
+
+NAME = os.path.basename(__file__).replace('.py', '')
+PATTERNS = [
+    '/Volumes/Jedi/Cache/221028_APL_10/*.tif',  # APL_144487 - APL_144807
+    '/Volumes/Jedi/Cache/221028_APL_11/*.tif',  # APL_144857 - APL_144977
+]
+# poster: APL_144857
+
+
+if __name__ == '__main__':
+    make_movie(NAME, PATTERNS, 24, 2, watermark=True, verbose=False, dryrun=False)
