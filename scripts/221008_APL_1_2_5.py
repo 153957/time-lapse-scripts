93973a33656a9f1d870020bbe92dc1467a9a2010
Arne de Laat <arne@delaat.net>
2022-12-26 12:44:16 +0100

    Add new time-lapse movie of Stevenskerk and ferris wheel Nijmegen

---
+++ scripts/221008_APL_1_2_5.py
@@ -0,0 +1,29 @@
+import os
+
+import ffmpeg
+
+from time_lapse import output
+
+NAME = os.path.basename(__file__).replace('.py', '')
+PATTERNS = [
+    ('/Volumes/Crimson/Cache/221008_Nijmegen/APL_1/*.tif', 48),  # APL_134754 - APL_135264
+    ('/Volumes/Crimson/Cache/221008_Nijmegen/APL_2/*.tif', 12),  # APL_135266 - APL_135384
+    ('/Volumes/Crimson/Cache/221008_Nijmegen/APL_5/*.tif', 24),  # APL_135838 - APL_136057
+]
+# poster: APL_136020
+
+
+def make_movie():
+    inputs = [
+        ffmpeg
+        .input(pattern, pattern_type='glob', framerate=framerate)
+        .filter_('deflicker', mode='pm', size=2)
+        for pattern, framerate in PATTERNS
+    ]
+    input = ffmpeg.concat(*inputs)
+
+    output.create_outputs(input, NAME, verbose=True, framerate=48)
+
+
+if __name__ == '__main__':
+    make_movie()
