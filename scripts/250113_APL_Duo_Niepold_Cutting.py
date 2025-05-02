22ace04fe52643ac1de589f7c3065bd6249e0539
Arne de Laat <arne@delaat.net>
2025-05-02 11:13:55 +0200

    Fix formatting

--- scripts/250113_APL_Duo_Niepold_Cutting.py
+++ scripts/250113_APL_Duo_Niepold_Cutting.py
@@ -14,9 +14,7 @@ POSTER = '/Volumes/Jedi/Cache/250113_APL_Duo_Niepold_Cutting/250113_APL_1_a/APL_
 
 
 if __name__ == '__main__':
-    inputs = [
-        source.get_input(pattern, fps=framerate, deflicker=0, filters=None) for pattern, framerate in PATTERNS
-    ]
+    inputs = [source.get_input(pattern, fps=framerate, deflicker=0, filters=None) for pattern, framerate in PATTERNS]
     combined_inputs = ffmpeg.concat(*inputs)
     output.create_outputs(combined_inputs, NAME, framerate=48, watermark=True, verbose=False, dryrun=False)
 
